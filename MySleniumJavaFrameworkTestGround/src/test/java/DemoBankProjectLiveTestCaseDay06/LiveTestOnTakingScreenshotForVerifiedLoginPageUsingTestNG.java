da355a08be24d085ce45cfb9dd434426f2754395
bappyasif <bappyasif@hotmail.com>
2019-04-22 00:46:40 +0600

    Today's Commit includes solution for Test Case 06 where we had to take a snapshot of successful verified login User home Page. Basically It's the same script from Yesterday except for few tweaks in and addition of takingSnapshot method, where Screenshot gets captured.

---
+++ MySleniumJavaFrameworkTestGround/src/test/java/DemoBankProjectLiveTestCaseDay06/LiveTestOnTakingScreenshotForVerifiedLoginPageUsingTestNG.java
@@ -0,0 +1,155 @@
+package DemoBankProjectLiveTestCaseDay06;
+
+import java.awt.Dimension;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LiveTestOnTakingScreenshotForVerifiedLoginPageUsingTestNG {
+
+	static WebDriver webDriver;
+	static String baseUrl = "http://www.demo.guru99.com/V4/";
+
+	@BeforeTest
+	public static void launchingBrowser() throws InterruptedException {
+
+		// Getting webDriver ready for this.
+		WebDriverManager.chromedriver().setup();
+		webDriver = new ChromeDriver();
+
+		// Navigating to test site's landing page
+		webDriver.get(baseUrl);
+		System.out.println("Landing Successful");
+		Thread.sleep(2000);
+	}
+
+
+	@Test(dataProvider = "getData")
+	public static void setData(String userID, String userPassword) throws InterruptedException {
+
+		System.out.println("ID Used: " +userID);
+		System.out.println("Password Used: " +userPassword);
+		System.out.println("Commencing Login :");
+		CommencingLogin(userID,userPassword);
+	}
+
+	@DataProvider
+	public Object[][] getData(){
+
+		//Rows - Number of times your test has to be repeated. "4"
+		//Columns - Number of parameters in test data. "2"
+		Object[][] data = new Object[4][2];
+
+		// 1st Row : Valid ID and Password
+		data[0][0] = "mngr190242";
+		data[0][1] = "hArEdAq";
+
+		// 2nd Row : InValid ID and Valid Password
+		data[1][0] = "mngr1136";
+		data[1][1] = "hArEdAq";
+
+		// 3rd Row : Valid ID and InValid Password
+		data[2][0] = "mngr190242";
+		data[2][1] = "hBr0dAq";
+
+		// 4th Row : InValid ID and InValid Password
+		data[3][0] = "mngr1137";
+		data[3][1] = "hBr0d0q";
+
+		return data;
+
+	}
+
+	public static void CommencingLogin(String ID, String PD) throws InterruptedException {
+
+		// Entering userID and Password to login section
+		webDriver.findElement(By.xpath("//input[@name='uid']")).clear();
+		webDriver.findElement(By.xpath("//input[@name='uid']")).sendKeys(ID);
+		webDriver.findElement(By.xpath("//input[@name='password']")).clear();
+		webDriver.findElement(By.xpath("//input[@name='password']")).sendKeys(PD);
+		webDriver.findElement(By.xpath("//input[@value='LOGIN']")).click();
+
+		System.out.println("Trying to Login Inside User Home Page");
+		Thread.sleep(2000);
+
+		// switching to new window. Even though it doesn't need this window switching but 
+		// still expert says its better this way when you're landing on a new URL.
+		for (String handle : webDriver.getWindowHandles()) {
+			webDriver.switchTo().window(handle);
+		}
+
+		// Verifying User login was successful
+		try {
+
+			System.out.println("Initiating Vefication Sequence Of User Home Page");
+
+			String userloginHeadingStatement = webDriver.findElement(By.xpath("//marquee[@class='heading3']")).getText().toString().trim();
+			System.out.println("Heading Statement : " +userloginHeadingStatement);
+
+			String userVerificationByID = webDriver.findElement(By.xpath("//td[contains(text(),'Manger Id : mngr190242')]")).getText().toString().trim();
+			System.out.println("User Identification : " +userVerificationByID);
+			
+			System.out.println("Calling TakingSnapshot");
+			TakingSnapshot();
+			System.out.println("Verification Done \n");
+			
+		} catch (Exception ex) {
+			// TODO: handle exception along with Alert Message.
+			System.out.println("Check Console For Clarification");
+			ex.getStackTrace();
+
+			Alert alert = webDriver.switchTo().alert();
+
+			String alertMessage = alert.getText().toString();
+			alert.accept();
+			System.out.println("Alert Box Message Is:  " +alertMessage +"\n");
+
+		} finally {
+
+			webDriver.get(baseUrl);
+		}
+	}
+	
+	
+	public static void TakingSnapshot() throws InterruptedException, IOException {
+		
+		org.openqa.selenium.Dimension windowSize = webDriver.manage().window().getSize(); 
+		webDriver.navigate().refresh();
+		Thread.sleep(2000);
+		webDriver.manage().window().maximize();
+		System.out.println("Taking ScreenShot");
+		
+		TakesScreenshot verifiedUserloginScreenShot = ((TakesScreenshot)webDriver);
+		File userLoggedInScreenShot = verifiedUserloginScreenShot.getScreenshotAs(OutputType.FILE);
+		String fileSaved = ("E:\\eclipse\\LiveTestCodeSamples\\Bank Demo\\VerifiedSuccessfulUserLoginScreenShot" +".png");
+		FileUtils.copyFile(userLoggedInScreenShot, new File(fileSaved));
+		
+		System.out.println("Your Screenshot Is Saved At This Location : " +fileSaved);
+		
+		webDriver.manage().window().setSize(windowSize);
+		System.out.println("Getting Out Of TakingSnapshot");
+		
+	}
+
+	
+	@AfterTest
+	public static void terminatingBrowser() {
+		
+		System.out.println("Test Completed...");
+		webDriver.quit();
+		
+	}
+}
