55b98b2e8268496f84958951a10f9a00a98dd773
bappyasif <bappyasif@hotmail.com>
2019-05-03 01:53:13 +0600

    Today's Commit Includes Solution For Test case Day 17,
    Where Editing Account details has to be verified.Even though Only One
    field was editable still managed to do some assertion with found details
    On Page with test Data That Is Provided.

--- MySleniumJavaFrameworkTestGround/src/test/java/DemoBankProjectLiveTestCaseDay17/LiveTestOnEditAccountAndVrification.java
+++ MySleniumJavaFrameworkTestGround/src/test/java/DemoBankProjectLiveTestCaseDay17/LiveTestOnEditAccountAndVrification.java
@@ -1,4 +1,6 @@
-package DemoBankProjectLiveTestCaseDay16;
+package DemoBankProjectLiveTestCaseDay17;
+
+import static org.testng.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,43 +13,39 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LiveTestOnCutomizedStatementAndVerification {
+public class LiveTestOnEditAccountAndVrification {
 
 	static WebDriver webDriver;	
 	static String baseUrl = "http://www.demo.guru99.com/V4/";
+
 	static final String userID = "mngr190242";
 	static final String userPassword = "hCrEd^8";
+	
 	static String accountID = "59372";
-	
-	static String fromDate = "04/01/2019";
-	static String toDate = "05/25/2019";
-	
-	static String minimumTransaction = "2000";
-	static String transactionNumbers = "30";
+	static String accountType = "Current";
 	
 	
 	public static void main(String[] args) {
 		
 		try {
 			
-			CommencingTestOnCustomizedStatementAndVerification();
+			CommencingTestOnEditingAccountAndVericiation();
 			
 		}catch (Exception ex) {
 			// TODO: handle exception
 			ex.getStackTrace();
-		} finally {
-			
+		}finally {
 			System.out.println("Test Completed...");
 			webDriver.quit();
-			
 		}
 	}
+	
 
-
-	public static void CommencingTestOnCustomizedStatementAndVerification() throws InterruptedException {
+	public static void CommencingTestOnEditingAccountAndVericiation() throws InterruptedException {
 
 		// Getting webDriver ready for this.
 		WebDriverManager.chromedriver().setup();
@@ -56,57 +54,73 @@ public class LiveTestOnCutomizedStatementAndVerification {
 		CommencingLogin();
 		System.out.println("Successful Login And verified \n");
 
-		CustommizedStatement();
+		EditAccount();
 		System.out.println("Edit Account Checkpoints Met \n");
 
 	}
 
 
-	public static void CustommizedStatement() throws InterruptedException {
-		
-		webDriver.findElement(By.linkText("Customised Statement")).click();
+	public static void EditAccount() throws InterruptedException {
+
+		webDriver.findElement(By.linkText("Edit Account")).click();
+
 		// switching to new window. Even though it doesn't need this window switching but 
 		// still expert says its better this way when you're landing on a new URL.
 		for(String windowHandler : webDriver.getWindowHandles()) {
 			webDriver.switchTo().window(windowHandler);
 		}
 
-		System.out.println("Inside Customized Statement Page");
+		System.out.println("Inside Edit Account Page");
 		Thread.sleep(2000);
-		
+
 		webDriver.findElement(By.xpath("//input[@name='accountno']")).clear();
 		webDriver.findElement(By.xpath("//input[@name='accountno']")).sendKeys(accountID);
-		webDriver.findElement(By.xpath("//input[@name='fdate']")).sendKeys(fromDate);
-		webDriver.findElement(By.xpath("//input[@name='tdate']")).sendKeys(toDate);
-		webDriver.findElement(By.xpath("//input[@name='amountlowerlimit']")).clear();
-		webDriver.findElement(By.xpath("//input[@name='amountlowerlimit']")).sendKeys(minimumTransaction);
-		webDriver.findElement(By.xpath("//input[@name='numtransaction']")).clear();
-		webDriver.findElement(By.xpath("//input[@name='numtransaction']")).sendKeys(transactionNumbers);
 		webDriver.findElement(By.xpath("//input[@name='AccSubmit']")).click();
-		
-		
+
+
 		// When Successful
 		try {
 			
-			String statementDetails = webDriver.findElement(By.xpath("//p[@class='heading3']")).getText().toString();
-			System.out.println("Message Shown : " +statementDetails);
+			String customerID = webDriver.findElement(By.xpath("//input[@name='txtcid']")).getText().toString();
 			
-			TakingSnapshot();
+			new Select(webDriver.findElement(By.xpath("//select[@name='a_type']"))).selectByVisibleText(accountType);
+			String amountShowing = webDriver.findElement(By.xpath("//input[@name='txtinitdep']")).getText().toString();
+			webDriver.findElement(By.xpath("//input[@name='AccSubmit']")).click();
+			
+			String updatedtype = webDriver.findElement(By.xpath("//tr[8]//td[2]")).getText().toString();
+			assertEquals(accountType, updatedtype);
+			System.out.println("Account Type Is Updated : " +updatedtype);
+			
+			//String customerFound = webDriver.findElement(By.xpath("//input[@name='txtcid']")).getText().toString();
+			//assertEquals(customerID, customerFound);
+			//System.out.println("Customer ID Is Verified" +customerID);
+			
+			String accountFound = webDriver.findElement(By.xpath("//td[contains(text(),'59372')]")).getText().toString();
+			assertEquals(accountID, accountFound);
+			System.out.println("Account ID Is Verified : " +accountFound);
+			
+			//String amountShown = webDriver.findElement(By.xpath("//td[contains(text(),'31792641')]")).getText().toString();
+			//assertEquals(amountShowing, amountShown);
+			//System.out.println("Total Amount Is Checked" +amountShowing);
+			
+			TakingSnapshot(); // When Changes are made.
 			
 			webDriver.findElement(By.linkText("Continue")).click();
-			System.out.println("Customized Statement Is Verified");
+			System.out.println("Verification Completed");
 			
+			HandlingAlert(); // When no changes are made.
+
 		}catch (Exception ex) {
 			// TODO: handle exception
 			ex.getStackTrace();
 		}
-		
-		
-		// When Unsuccessful
+
+
+		// When Unsuccessful or No changes Are Made
 		try {
-			
+
 			HandlingAlert();
-			
+
 		}catch (Exception ex) {
 			// TODO: handle exception
 			ex.getStackTrace();
@@ -127,10 +141,10 @@ public class LiveTestOnCutomizedStatementAndVerification {
 		webDriver.manage().window().maximize();
 		System.out.println("Taking ScreenShot");
 
-		TakesScreenshot customizedStatementVerificationScreenShot = ((TakesScreenshot)webDriver);
-		File csScreenShot = customizedStatementVerificationScreenShot.getScreenshotAs(OutputType.FILE);
-		String fileSaved = ("E:\\eclipse\\LiveTestCodeSamples\\Bank Demo\\CustomizedStatementVerificationScreenShot" +".png");
-		FileUtils.copyFile(csScreenShot, new File(fileSaved));
+		TakesScreenshot edittedAccountVerificationScreenShot = ((TakesScreenshot)webDriver);
+		File eavScreenShot = edittedAccountVerificationScreenShot.getScreenshotAs(OutputType.FILE);
+		String fileSaved = ("E:\\eclipse\\LiveTestCodeSamples\\Bank Demo\\EdittedAccountVerificationScreenShot" +".png");
+		FileUtils.copyFile(eavScreenShot, new File(fileSaved));
 
 		System.out.println("Your Screenshot Is Saved At This Location : " +fileSaved);
 
@@ -139,8 +153,7 @@ public class LiveTestOnCutomizedStatementAndVerification {
 
 	}
 
-	
-	
+
 	public static void HandlingAlert() {
 
 		Alert alert = webDriver.switchTo().alert();
@@ -198,4 +211,5 @@ public class LiveTestOnCutomizedStatementAndVerification {
 			System.out.println("Alert Box Message Is:  " +alertMessage +"\n");
 		}
 	}
+
 }
